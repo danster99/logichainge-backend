305a63659e6dea506d92cddfe997f700dd6472ae
Dan Niculae <danandreiniculae@yahoo.com>
2023-01-30 17:11:14 +0200

    added file for database cleanup

---
+++ sql truncate and restart seq.sql
@@ -0,0 +1,20 @@
+TRUNCATE 
+employee,
+department,
+contact,
+client,
+image,
+attachment,
+goods,
+activity ,
+transport_file
+;
+ALTER SEQUENCE employee_id_seq RESTART WITH 1;
+ALTER SEQUENCE department_id_seq RESTART WITH 1;
+ALTER SEQUENCE contact_id_seq RESTART WITH 1;
+ALTER SEQUENCE client_id_seq RESTART WITH 1;
+ALTER SEQUENCE image_id_seq RESTART WITH 1;
+ALTER SEQUENCE attachment_id_seq RESTART WITH 1;
+ALTER SEQUENCE good_id_seq RESTART WITH 1;
+ALTER SEQUENCE activity_id_seq RESTART WITH 1;
+ALTER SEQUENCE transport_file_id_seq RESTART WITH 1;
